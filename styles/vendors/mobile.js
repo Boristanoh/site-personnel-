945270cb32ac193f87ae7bc13c88b9601b64ae31
Boris TANOH <138991507+Boristanoh@users.noreply.github.com>
2023-12-05 04:01:09 +0000

    debut
    initialisation

---
+++ styles/vendors/mobile.js
@@ -0,0 +1,24 @@
+
+             
+ 
+document.getElementById('mobile-menu').addEventListener('click', function() {
+  
+
+   var element = document.querySelector('.navcolumn');
+   element.classList.toggle('visible');
+   
+});
+   
+
+
+
+           
+
+   document.getElementById('mobile-menu-exit').addEventListener('click', function() {
+   
+
+   var element = document.querySelector('.navcolumn');
+   element.classList.toggle('visible');
+   
+});
+   
