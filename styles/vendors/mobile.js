63858897aca61b381717ae1d4e55124e0997870e
Boris TANOH <138991507+Boristanoh@users.noreply.github.com>
2023-12-05 13:49:54 +0000

    contatct

--- styles/vendors/mobile.js
+++ styles/vendors/mobile.js
@@ -6,6 +6,16 @@ document.getElementById('mobile-menu').addEventListener('click', function() {
 
    var element = document.querySelector('.navcolumn');
    element.classList.toggle('visible');
+
+   var monElement = document.getElementById('map');
+   
+      monElement.style.display = 'none';
+      console.log('ok')
+      
+   
+
+// Changement de la propriété display à "none" pour le cacher
+
    
 });
    
@@ -19,6 +29,10 @@ document.getElementById('mobile-menu').addEventListener('click', function() {
 
    var element = document.querySelector('.navcolumn');
    element.classList.toggle('visible');
+   var monElement = document.getElementById('map');
+   
+      monElement.style.display = 'block';
+      console.log('ok')
    
 });
    
