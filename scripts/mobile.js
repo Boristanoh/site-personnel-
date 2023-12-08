4527e6e21873758dfd095d840702ea9aff58294f
Boris TANOH <138991507+Boristanoh@users.noreply.github.com>
2023-12-08 11:22:38 +0000

    defilement

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -9,33 +9,39 @@ document.getElementById('mobile-menu').addEventListener('click', function() {
 
    var monElement = document.getElementById('map');
    if (monElement){
+    
       monElement.style.display = 'none';
       console.log('ok')
    }
    var infoAccueil = document.querySelector('.infoaccueil');
       if (infoAccueil){
          infoAccueil.style.visibility = 'hidden';
+         infoAccueil.style.overflow = 'hidden';
          console.log('ok none')
    }
    var infoAbout = document.querySelector('.infoabout');
    if (infoAbout){
       infoAbout.style.visibility = 'hidden';
+      infoAbout.style.overflow = 'hidden';
       console.log('ok none')
 }
    var infoPacours = document.querySelector('.infoparcours');
    if (infoPacours ){
       infoPacours .style.visibility = 'hidden';
+      infoPacours.style.overflow = 'hidden';
       console.log('ok none')
    }
 
    var infoPortfolio = document.querySelector('.infoportfolio');
    if (infoPortfolio){
       infoPortfolio.style.visibility = 'hidden';
+      infoPortfolio.style.overflow = 'hidden';
       console.log('ok none')
    }
    var infoContact = document.querySelector('.infocontact');
    if (infoContact){
       infoContact.style.visibility = 'hidden';
+      infoContact.style.overflow = 'hidden';
       console.log('ok none')
    }
       
@@ -65,29 +71,34 @@ document.getElementById('mobile-menu').addEventListener('click', function() {
    var infoAccueil = document.querySelector('.infoaccueil');
       if (infoAccueil){
          infoAccueil.style.visibility = 'visible';
+         infoAccueil.style.overflow = 'auto';
          console.log('ok')
    }
 
    var infoAbout = document.querySelector('.infoabout');
    if (infoAbout){
       infoAbout.style.visibility = 'visible';
+      infoAbout.style.overflow = 'auto';
       console.log('ok')
 }
 
    var infoPacours = document.querySelector('.infoparcours');
    if (infoPacours ){
       infoPacours .style.visibility = 'visible';
+      infoPacours.style.overflow = 'auto';
       console.log('ok')
    }
    
    var infoPortfolio = document.querySelector('.infoportfolio');
    if (infoPortfolio){
       infoPortfolio.style.visibility = 'visible';
+      infoPortfolio.style.overflow = 'auto';
       console.log('ok')
 }
    var infoContact = document.querySelector('.infocontact');
    if (infoContact){
       infoContact.style.visibility = 'visible';
+      infoContact.style.overflow = 'auto';
       console.log('ok')
    }
 });
